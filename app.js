da7ae7aa4673b177f3aed02ae9fa08d2319696d8
skekw <sug71.4.7002@outlook.jp>
2025-08-02 05:12:25 +0900

    add env

--- app.js
+++ app.js
@@ -8,12 +8,20 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
+var catRouter = require('./routes/cat');
+var qrRouter = require('./routes/qr');
+var notes_from_bRouter = require('./routes/notes_from_b');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use('/hello', helloRouter);
+app.use('/notes', notesRouter);
+app.use('/cat', catRouter);
+app.use('/qr', qrRouter);
+app.use('/notes_from_b', notes_from_bRouter);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,8 +31,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/hello', helloRouter);
-app.use('/notes', notesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
